c9436e65d3bcbc9c1d6ad759f9e0a12c9186c8e1
RJ Bruneel <ra368420@cmd25nm06nf8j9.net.ucf.edu>
2017-04-07 16:50:13 -0400

    Added logic for the three layouts

--- includes/ucf-promo-common.php
+++ includes/ucf-promo-common.php
@@ -6,23 +6,66 @@
 if ( ! class_exists( 'UCF_Promo_Common' ) ) {
 	class UCF_Promo_Common {
 		public static function display_promo( $layout ) {
-
-			ob_start();
-
-			$output = ucf_promo_display();
+			$output = ucf_promo_display( $layout );
 			echo apply_filters( 'ucf_promo_display', $output );
-
-			return ob_get_clean();
-
 		}
 	}
 }
 
 if ( ! function_exists( 'ucf_promo_display' ) ) {
-	function ucf_promo_display( $output='' ) {
+	function ucf_promo_display( $layout='' ) {
+		switch ($layout) {
+			case "horizontal":
+				echo ucf_promo_horizontal();
+				break;
+			case "vertical":
+				echo ucf_promo_vertical();
+				break;
+			case "square":
+				echo ucf_promo_square();
+				break;
+		}
+	}
+}
+
+if ( ! function_exists( 'ucf_promo_horizontal' ) ) {
+	function ucf_promo_horizontal( ) {
 		ob_start();
 	?>
-		<h2>Promo Goes Here</h2>
+		<div>
+			<h3>Title</h3>
+			<p>Description</p>
+			<a href="">Link</a>
+		</div>
+	<?php
+		return ob_get_clean();
+	}
+}
+
+if ( ! function_exists( 'ucf_promo_vertical' ) ) {
+	function ucf_promo_vertical( ) {
+		ob_start();
+	?>
+		<div>
+			<img src="" alt="">
+
+			<h3>Title</h3>
+			<p>Description</p>
+			<a href="" class="btn btn-gold">Button</a>
+		</div>
+	<?php
+		return ob_get_clean();
+	}
+}
+
+if ( ! function_exists( 'ucf_promo_square' ) ) {
+	function ucf_promo_square( ) {
+		ob_start();
+	?>
+		<div>
+			<p>Square copy</p>
+			<a href="" class="btn btn-gold">Button</a>
+		</div>
 	<?php
 		return ob_get_clean();
 	}
--- shortcodes/ucf-promo-shortcode.php
+++ shortcodes/ucf-promo-shortcode.php
@@ -1,15 +1,18 @@
-
 <?php
 /**
  * Registers the promo shortcode
  **/
-class UCF_Promo_Shortcode {
-	public static function shortcode( $atts ) {
-		$atts = shortcode_atts( array(
-			'layout'       => '',
-		), $atts );
-		return UCF_Promo_Common::display_promo( $atts['layout'] );
+
+if ( ! class_exists( 'UCF_Promo_Shortcode' ) ) {
+	class UCF_Promo_Shortcode {
+		public static function shortcode( $atts ) {
+			$atts = shortcode_atts( array(
+				'layout'       => '',
+			), $atts );
+
+			return UCF_Promo_Common::display_promo( $atts['layout'] );
+		}
 	}
+	add_shortcode( 'ucf-promo', array( 'UCF_Promo_Shortcode', 'shortcode' ) );
 }
-add_shortcode( 'ucf-promo', array( 'UCF_Promo_Shortcode', 'shortcode' ) );
 ?>
